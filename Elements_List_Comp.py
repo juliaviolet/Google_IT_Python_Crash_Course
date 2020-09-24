565c533128e7da37b10fa0b7bbbc632f623e644e
juliaviolet <juliacarolineviolet@gmail.com>
2020-09-24 16:35:58 -0400

    Create Elements_List_Comp.py

---
+++ Elements_List_Comp.py
@@ -0,0 +1,7 @@
+def skip_elements(elements):
+  res = [elements[i] for i in range (len(elements)) if i%2==0]
+
+  return str(res)
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
